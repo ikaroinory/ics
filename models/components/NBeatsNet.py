ee2f5193e5a04592faf2eb4dde4dbf771475c400
Guanyu Hu <ikaroinory@gmail.com>
2025-03-27 14:10:22 +0800

    remove device par in NBeats

--- models/components/NBeatsNet.py
+++ models/components/NBeatsNet.py
@@ -16,7 +16,6 @@ class NBeatsNet(nn.Module):
 
     def __init__(
         self,
-        device=torch.device('cpu'),
         stack_types=(TREND_BLOCK, SEASONALITY_BLOCK),
         nb_blocks_per_stack=3,
         forecast_length=5,
@@ -37,11 +36,9 @@ class NBeatsNet(nn.Module):
         self.stacks = []
         self.thetas_dim = thetas_dim
         self.parameters = []
-        self.device = device
         for stack_id in range(len(self.stack_types)):
             self.stacks.append(self.create_stack(stack_id))
         self.parameters = nn.ParameterList(self.parameters)
-        self.to(self.device)
         self._loss = None
         self._opt = None
         self._gen_intermediate_outputs = False
@@ -57,7 +54,7 @@ class NBeatsNet(nn.Module):
             else:
                 block = block_init(
                     self.hidden_layer_units, self.thetas_dim[stack_id],
-                    self.device, self.backcast_length, self.forecast_length,
+                    self.backcast_length, self.forecast_length,
                     self.nb_harmonics
                 )
                 self.parameters.extend(block.parameters())
@@ -126,8 +123,8 @@ class NBeatsNet(nn.Module):
         for stack_id in range(len(self.stacks)):
             for block_id in range(len(self.stacks[stack_id])):
                 b, f = self.stacks[stack_id][block_id](backcast)
-                backcast = backcast.to(self.device) - b
-                forecast = forecast.to(self.device) + f
+                backcast = backcast.to() - b
+                forecast = forecast.to() + f
                 block_type = self.stacks[stack_id][block_id].__class__.__name__
                 layer_name = f'stack_{stack_id}-{block_type}_{block_id}'
                 if self._gen_intermediate_outputs:
@@ -141,21 +138,21 @@ def squeeze_last_dim(tensor):
     return tensor
 
 
-def seasonality_model(thetas, t, device):
+def seasonality_model(thetas, t):
     p = thetas.size()[-1]
     assert p <= thetas.shape[1], 'thetas_dim is too big.'
     p1, p2 = (p // 2, p // 2) if p % 2 == 0 else (p // 2, p // 2 + 1)
     s1 = torch.tensor(np.array([np.cos(2 * np.pi * i * t) for i in range(p1)])).float()  # H/2-1
     s2 = torch.tensor(np.array([np.sin(2 * np.pi * i * t) for i in range(p2)])).float()
     S = torch.cat([s1, s2])
-    return thetas.mm(S.to(device))
+    return thetas.mm(S)
 
 
-def trend_model(thetas, t, device):
+def trend_model(thetas, t):
     p = thetas.size()[-1]
     assert p <= 4, 'thetas_dim is too big.'
     T = torch.tensor(np.array([t ** i for i in range(p)])).float()
-    return thetas.mm(T.to(device))
+    return thetas.mm(T)
 
 
 def linear_space(backcast_length, forecast_length, is_forecast=True):
@@ -166,7 +163,7 @@ def linear_space(backcast_length, forecast_length, is_forecast=True):
 class Block(nn.Module):
 
     def __init__(
-        self, units, thetas_dim, device, backcast_length=10, forecast_length=5, share_thetas=False,
+        self, units, thetas_dim, backcast_length=10, forecast_length=5, share_thetas=False,
         nb_harmonics=None
     ):
         super(Block, self).__init__()
@@ -179,7 +176,6 @@ class Block(nn.Module):
         self.fc2 = nn.Linear(units, units)
         self.fc3 = nn.Linear(units, units)
         self.fc4 = nn.Linear(units, units)
-        self.device = device
         self.backcast_linspace = linear_space(backcast_length, forecast_length, is_forecast=False)
         self.forecast_linspace = linear_space(backcast_length, forecast_length, is_forecast=True)
         if share_thetas:
@@ -190,7 +186,7 @@ class Block(nn.Module):
 
     def forward(self, x):
         x = squeeze_last_dim(x)
-        x = F.relu(self.fc1(x.to(self.device)))
+        x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
         x = F.relu(self.fc4(x))
@@ -205,38 +201,38 @@ class Block(nn.Module):
 
 class SeasonalityBlock(Block):
 
-    def __init__(self, units, thetas_dim, device, backcast_length=10, forecast_length=5, nb_harmonics=None):
+    def __init__(self, units, thetas_dim, backcast_length=10, forecast_length=5, nb_harmonics=None):
         if nb_harmonics:
-            super(SeasonalityBlock, self).__init__(units, nb_harmonics, device, backcast_length,
+            super(SeasonalityBlock, self).__init__(units, nb_harmonics, backcast_length,
                                                    forecast_length, share_thetas=True)
         else:
-            super(SeasonalityBlock, self).__init__(units, forecast_length, device, backcast_length,
+            super(SeasonalityBlock, self).__init__(units, forecast_length, backcast_length,
                                                    forecast_length, share_thetas=True)
 
     def forward(self, x):
         x = super(SeasonalityBlock, self).forward(x)
-        backcast = seasonality_model(self.theta_b_fc(x), self.backcast_linspace, self.device)
-        forecast = seasonality_model(self.theta_f_fc(x), self.forecast_linspace, self.device)
+        backcast = seasonality_model(self.theta_b_fc(x), self.backcast_linspace)
+        forecast = seasonality_model(self.theta_f_fc(x), self.forecast_linspace)
         return backcast, forecast
 
 
 class TrendBlock(Block):
 
-    def __init__(self, units, thetas_dim, device, backcast_length=10, forecast_length=5, nb_harmonics=None):
-        super(TrendBlock, self).__init__(units, thetas_dim, device, backcast_length,
+    def __init__(self, units, thetas_dim, backcast_length=10, forecast_length=5, nb_harmonics=None):
+        super(TrendBlock, self).__init__(units, thetas_dim, backcast_length,
                                          forecast_length, share_thetas=True)
 
     def forward(self, x):
         x = super(TrendBlock, self).forward(x)
-        backcast = trend_model(self.theta_b_fc(x), self.backcast_linspace, self.device)
-        forecast = trend_model(self.theta_f_fc(x), self.forecast_linspace, self.device)
+        backcast = trend_model(self.theta_b_fc(x), self.backcast_linspace)
+        forecast = trend_model(self.theta_f_fc(x), self.forecast_linspace)
         return backcast, forecast
 
 
 class GenericBlock(Block):
 
-    def __init__(self, units, thetas_dim, device, backcast_length=10, forecast_length=5, nb_harmonics=None):
-        super(GenericBlock, self).__init__(units, thetas_dim, device, backcast_length, forecast_length)
+    def __init__(self, units, thetas_dim, backcast_length=10, forecast_length=5, nb_harmonics=None):
+        super(GenericBlock, self).__init__(units, thetas_dim, backcast_length, forecast_length)
 
         self.backcast_fc = nn.Linear(thetas_dim, backcast_length)
         self.forecast_fc = nn.Linear(thetas_dim, forecast_length)
